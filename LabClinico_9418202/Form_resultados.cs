9789a5393667bce6257f820bd55e5be54b78e470
EmilioOlivares <made.in.lord@gmail.com>
2020-07-29 23:42:36 -0400

    cambios en clase conexion y form resultado

--- LabClinico_9418202/Form_resultados.cs
+++ LabClinico_9418202/Form_resultados.cs
@@ -16,7 +16,14 @@ namespace LabClinico_9418202
         }
 
         private void Form_resultados_Load(object sender, EventArgs e) {
+            C.conectar();
             C.llenar_rut(cbx_rut);
+            C.llenar_cod_med(cbx_m);
+            C.llenar_cod_tec(cbx_t);
+            C.llenar_diag(cbx_d1);
+            C.llenar_diag(cbx_d2);
+            C.llenar_origen(cbx_o);
+            C.desconectar();
         }
     }
 }
